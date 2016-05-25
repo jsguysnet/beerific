5fbf75638f4dd672cb41df6bdb80b5349366ee6f
= <maikpaulus@web.de>
2016-05-25 23:46:37 +0200

    - added db connection to server - added search to beergarden list

--- www/js/beerific.js
+++ www/js/beerific.js
@@ -1,20 +1,46 @@
 var Beerific = {
+    EARTH_RADIUS: 6378.388,
+
     getData: function (filter, callback) {
-        console.log(filter);
-        var url = 'http://api.beerific.jsguys.net/beergarden';
+        var self = this;
+        var url = 'http://api.beerific.jsguys.net';
+
+        var lat = 48.090180;
+        var lng = 11.497929;
 
         $.ajax({
             url: url,
             method: 'POST',
+            data: {
+                query: 'SELECT * FROM v_beergarden_list'
+            },
 
             success: function (data, status) {
                 if (data.success) {
-                    callback(data.data);
+                    var beergarden = data.data;
+
+                    var items = data.data.map(function (item) {
+                        item['distance'] = self._getDistance(lat, lng, item.latitude, item.longitude, 1);
+                        return item;
+                    });
+                    
+                    callback(items);
                 }
             }
         });
     },
-    
+
+    _getDistance: function (lat1, lng1, lat2, lng2, accuracy) {
+        var self = this;
+
+        var latProd = Math.sin(lat1) * Math.sin(lat2);
+        var lngProd = Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1);
+        var acc = accuracy ? Math.pow(10, accuracy) : 10;
+
+        distance = self.EARTH_RADIUS * Math.acos(latProd + lngProd) * (Math.PI / 180);
+        return Math.round(distance * acc) / acc;
+    },
+
     createDetails: function (beer) {
         document.getElementsByClassName('title')[0].innerHTML = beer.title;
         document.getElementsByClassName('description')[0].innerHTML = beer.description;
